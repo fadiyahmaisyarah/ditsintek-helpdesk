fcdf632c375c4404825385e27ff2164bf9837934
Fadiyah Maisyarah <fadiyahmaisyarah.yaya@gmail.com>
2026-07-23 14:35:38 +0700

    set BASE_URL to railway backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// Base URL REST API akan diisi oleh tim backend nanti, contoh lewat file .env:
-// VITE_API_BASE_URL=https://api.helpdesk.ditsintek.usu.ac.id
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+// Tetap tambahkan https:// 
+const BASE_URL = 'https://helpdesk-ditsintek-backend-production.up.railway.app';
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -10,12 +9,5 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-// Interceptor contoh untuk menyisipkan token auth nanti, misal:
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
 
 export default api;
