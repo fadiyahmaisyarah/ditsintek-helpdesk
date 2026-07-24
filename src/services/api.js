3afa5425f1864d837b3157ab61699e5ece3b1741
Fadiyah Maisyarah <fadiyahmaisyarah.yaya@gmail.com>
2026-07-24 10:57:13 +0700

    fix: update baseURL with /api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://helpdesk-ditsintek-backend.vercel.app', 
+  baseURL: 'https://helpdesk-ditsintek-backend.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
